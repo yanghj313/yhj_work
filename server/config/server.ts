d93d29492107b47f3001d67011aef189c1bc743b
yhj <lawork313@gmail.com>
2025-04-10 09:24:55 +0900

    추가 수정

--- server/config/server.ts
+++ server/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
-  host: env('HOST', '127.0.0.1'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS', ['yourAppKey1', 'yourAppKey2']),
+    keys: env.array('APP_KEYS'),
   },
 });
