f956cb64624b756a4741f792f9432e751aa58e44
yhj <lawork313@gmail.com>
2025-04-10 18:05:19 +0900

    커밋밋

--- server/config/database.ts
+++ server/config/database.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 
-export default ({ env }: { env: any }) => {
-  const client = env('DATABASE_CLIENT', 'postgres'); // PostgreSQL 사용
+export default ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'sqlite');
 
   const connections = {
     mysql: {
@@ -24,16 +24,29 @@ export default ({ env }: { env: any }) => {
     },
     postgres: {
       connection: {
-        connectionString: env('DATABASE_URL'), // PostgreSQL URL 사용
+        connectionString: env('DATABASE_URL'),
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
         ssl: env.bool('DATABASE_SSL', false) && {
+          key: env('DATABASE_SSL_KEY', undefined),
+          cert: env('DATABASE_SSL_CERT', undefined),
+          ca: env('DATABASE_SSL_CA', undefined),
+          capath: env('DATABASE_SSL_CAPATH', undefined),
+          cipher: env('DATABASE_SSL_CIPHER', undefined),
           rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
-      pool: {
-        min: env.int('DATABASE_POOL_MIN', 0),
-        max: env.int('DATABASE_POOL_MAX', 5),
+      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+    },
+    sqlite: {
+      connection: {
+        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
       },
+      useNullAsDefault: true,
     },
   };
 
