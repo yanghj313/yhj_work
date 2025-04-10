edd500e6b7ccbc58d4dbb590f0ef2f70c5d73d36
yhj <lawork313@gmail.com>
2025-04-10 18:29:14 +0900

    미들웨어 수정정

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -1,12 +1,52 @@
 export default [
+  // 로그 미들웨어
   'strapi::logger',
+
+  // 에러 처리 미들웨어
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+
+  // 보안 관련 설정 미들웨어
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'default-src': ["'self'"],
+          'img-src': ["'self'", 'data:', 'https://yourwebsite.com'],
+          'script-src': ["'self'", "'unsafe-inline'"],
+        },
+      },
+    },
+  },
+
+  // CORS 설정 미들웨어
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['*'], // 모든 도메인 허용 (특정 도메인만 허용하려면 ['https://yourdomain.com'])
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: ['Content-Type', 'Authorization', 'Origin'],
+      exposeHeaders: ['X-Total-Count'],
+    },
+  },
+
+  // PoweredBy 헤더 미들웨어
   'strapi::poweredBy',
+
+  // 쿼리 관련 설정 미들웨어
   'strapi::query',
+
+  // 바디 파싱 미들웨어
   'strapi::body',
+
+  // 세션 관련 미들웨어
   'strapi::session',
+
+  // 파비콘 설정 미들웨어
   'strapi::favicon',
+
+  // 정적 파일 관련 미들웨어
   'strapi::public',
 ];
