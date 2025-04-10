606f06ee5c74ac94e6450c8d731d73a99a68dba9
yhj <lawork313@gmail.com>
2025-04-10 15:12:38 +0900

    수정정

--- server/config/admin.ts
+++ server/config/admin.ts
@@ -1,17 +1,13 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'someAdminJwtSecret'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'yourVerySecretSalt'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'yourSuperSecretTransferSalt'),
     },
-  },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
 });
--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -1,8 +1,17 @@
+// config/middlewares.ts or config/middlewares.js
 export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:5173', 'https://yhjwork-production.up.railway.app'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
