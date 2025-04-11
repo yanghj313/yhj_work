d184146aa98c1919d1960b84ea1ddb9ee40d9e95
yhj <lawork313@gmail.com>
2025-04-11 13:45:35 +0900

    링크 수정

--- client/src/components/ProjectList.jsx
+++ client/src/components/ProjectList.jsx
@@ -41,7 +41,7 @@ const ProjectList = () => {
               )}
 
               <strong>
-                <Link to={`/projects/${p.id}`}>{p.title}</Link>
+                <Link to={`/projects/${p.documentId}`}>{p.title}</Link>
               </strong>
               <br />
 
