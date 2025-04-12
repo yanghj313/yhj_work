6fa8b6acdc2de80dcc38cd17965b7d7410d0b4a2
yanghj313 <lawork313@gmail.com>
2025-04-12 23:47:37 +0900

    효과 넣기

--- client/src/components/FullpageHome.jsx
+++ client/src/components/FullpageHome.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Lenis from '@studio-freight/lenis';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
