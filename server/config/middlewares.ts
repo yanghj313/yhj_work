dcdb034b07fb0d6947dc212dc31a4328cc3afea9
yanghj313 <lawork313@gmail.com>
2025-04-13 08:35:44 +0900

    미들웨어 수정

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -1,58 +1,43 @@
 export default [
-  // 로그 미들웨어
-  'strapi::logger',
+	'strapi::logger',
+	'strapi::errors',
 
-  // 에러 처리 미들웨어
-  'strapi::errors',
+	{
+		name: 'strapi::security',
+		config: {
+			contentSecurityPolicy: {
+				useDefaults: true,
+				directives: {
+					'default-src': ["'self'"],
+					'img-src': ["'self'", 'data:', 'https://yhj-work.vercel.app'],
+					'script-src': ["'self'", "'unsafe-inline'", 'https://yhj-work.vercel.app'],
+					'frame-src': ["'self'", 'https://yhj-work.vercel.app'],
+				},
+			},
+		},
+	},
 
-  // 보안 관련 설정 미들웨어 (CSP)
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'default-src': ["'self'"],
-          'img-src': ["'self'", 'data:', 'https://yhj-work.vercel.app'],
-          'script-src': ["'self'", "'unsafe-inline'", 'https://yhj-work.vercel.app'],
-          'frame-src': ["'self'", 'https://yhj-work.vercel.app'],
-        },
-      },
-    },
-  },
+	{
+		name: 'strapi::cors',
+		config: {
+			enabled: true,
+			origin: [
+				'https://yhj-work.vercel.app',
+				'https://yhj-work-git-main-yanghj313s-projects.vercel.app',
+				'https://yhj-work-yanghj313s-projects.vercel.app',
+				'http://localhost:3000',
+				'http://localhost:5173',
+			],
+			methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+			headers: ['Content-Type', 'Authorization'],
+			credentials: true,
+		},
+	},
 
-  // CORS 설정 미들웨어
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      origin: [
-        'https://yhj-work.vercel.app',
-        'https://yhj-work-git-main-yanghj313s-projects.vercel.app',
-        'http://localhost:3000',
-        'http://localhost:5173'
-      ],
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
-      headers: ['Content-Type', 'Authorization'],
-      credentials: true,
-    },
-  },
-
-  // PoweredBy 헤더 미들웨어
-  'strapi::poweredBy',
-
-  // 쿼리 관련 설정 미들웨어
-  'strapi::query',
-
-  // 바디 파싱 미들웨어
-  'strapi::body',
-
-  // 세션 관련 미들웨어
-  'strapi::session',
-
-  // 파비콘 설정 미들웨어
-  'strapi::favicon',
-
-  // 정적 파일 관련 미들웨어
-  'strapi::public',
+	'strapi::poweredBy',
+	'strapi::query',
+	'strapi::body',
+	'strapi::session',
+	'strapi::favicon',
+	'strapi::public',
 ];
