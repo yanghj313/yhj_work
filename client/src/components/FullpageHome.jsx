a4efb40d5a4da8aa281a4c39cfb230e92c7b4f30
yanghj313 <lawork313@gmail.com>
2025-04-12 19:57:18 +0900

    메인 글자 효과

--- client/src/components/FullpageHome.jsx
+++ client/src/components/FullpageHome.jsx
@@ -18,22 +18,43 @@ const FullpageHome = () => {
 	const scrollRef = useRef(null);
 
 	useEffect(() => {
+		// 텍스트 스플리팅
 		Splitting();
-		ScrollOut({ targets: '.word' });
 
+		// 스크롤 라이브러리 초기화
 		const scroll = new LocomotiveScroll({
 			el: scrollRef.current,
 			smooth: true,
 			lerp: 0.07,
 		});
 
-		return () => scroll.destroy();
+		// ScrollOut과 연결
+		ScrollOut({
+			targets: '.word',
+			scrollingElement: scrollRef.current,
+			onShown: el => el.setAttribute('data-scroll', 'in'),
+			onHidden: el => el.setAttribute('data-scroll', 'out'),
+		});
+
+		return () => {
+			scroll.destroy();
+		};
 	}, []);
 
 	return (
 		<div ref={scrollRef} data-scroll-container>
 			{sectionTexts.map((section, i) => (
-				<section key={i} className={`panel section`} style={{ minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }} data-scroll-section>
+				<section
+					key={i}
+					className="panel section"
+					data-scroll-section
+					style={{
+						minHeight: '100vh',
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
 					<div className={`text text--${section.effect} word`} data-scroll data-splitting>
 						{section.text}
 					</div>
