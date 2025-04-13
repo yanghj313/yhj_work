1701af94a1dd951ac98193d978e9671f58ee9ce8
yanghj313 <lawork313@gmail.com>
2025-04-13 15:07:56 +0900

    섹션수정

--- client/src/components/InterestBubbleChart.jsx
+++ client/src/components/InterestBubbleChart.jsx
@@ -118,7 +118,7 @@ const InterestBubbleChart = () => {
 	const isMobile = dimensions.width <= 1024;
 
 	return (
-		<div className="interest-chart-wrapper" style={{ position: 'relative' }}>
+		<div className="interest-section" style={{ position: 'relative' }}>
 			<div className="bubble-chart">
 				<svg ref={svgRef} className="img-chart"></svg>
 			</div>
