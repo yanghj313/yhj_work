7144141344f7d1820ad7ba9460d90e902ce8c57e
yhj <lawork313@gmail.com>
2025-04-14 12:45:26 +0900

    소개 페이지 수정정

--- client/src/components/Introduction.jsx
+++ client/src/components/Introduction.jsx
@@ -1,1 +1,11 @@
+import React from 'react';
 
+const Introduction = () => {
+	return (
+		<div className="section two">
+			<h1>Introduction Section</h1>
+		</div>
+	);
+};
+
+export default Introduction;
