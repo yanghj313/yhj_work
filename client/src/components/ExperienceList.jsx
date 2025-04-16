bb27c72d5b62c2478e83438e83f7037f48ab15e6
yhj <lawork313@gmail.com>
2025-04-16 18:29:38 +0900

    page css 수정정

--- client/src/components/ExperienceList.jsx
+++ client/src/components/ExperienceList.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import '../assets/css/page.css';
 
 const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:1337';
 
