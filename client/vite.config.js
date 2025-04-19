2f086889369f0c227037b3af654cd0f178e325db
yanghj313 <lawork313@gmail.com>
2025-04-19 11:25:28 +0900

    디버깅용 파일 정리

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		sourcemap: true, // ✅ 이걸 꼭 true로 설정해야 배포본에 소스맵이 포함됨
+		rollupOptions: {
+			output: {
+				// 파일 단위로 나뉘게끔 설정 가능 (선택 사항)
+				chunkFileNames: 'chunks/[name]-[hash].js',
+				entryFileNames: 'entries/[name]-[hash].js',
+				assetFileNames: 'assets/[name]-[hash].[ext]',
+			},
+		},
+	},
 	css: {
 		devSourcemap: true,
 	},
