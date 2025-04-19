1fbb82188100cdc75048b3c748248ecfae02784c
yanghj313 <lawork313@gmail.com>
2025-04-19 11:32:13 +0900

    파일 디버깅용 쪼개기

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,18 +4,23 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 	plugins: [react()],
 	build: {
-		sourcemap: true, // ✅ 이걸 꼭 true로 설정해야 배포본에 소스맵이 포함됨
+		sourcemap: true, // ✅ JS, CSS map 파일 생성
+		cssCodeSplit: true, // ✅ CSS 파일을 컴포넌트 단위로 분리
 		rollupOptions: {
 			output: {
-				// 파일 단위로 나뉘게끔 설정 가능 (선택 사항)
-				chunkFileNames: 'chunks/[name]-[hash].js',
-				entryFileNames: 'entries/[name]-[hash].js',
-				assetFileNames: 'assets/[name]-[hash].[ext]',
+				entryFileNames: 'entries/[name]-[hash].js', // JS 엔트리
+				chunkFileNames: 'chunks/[name]-[hash].js', // JS 청크
+				assetFileNames: assetInfo => {
+					if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+						return 'styles/[name]-[hash][extname]'; // ✅ CSS 파일은 styles 폴더로
+					}
+					return 'assets/[name]-[hash][extname]'; // 나머지 자산
+				},
 			},
 		},
 	},
 	css: {
-		devSourcemap: true,
+		devSourcemap: true, // ✅ 개발환경에서도 CSS map 사용
 	},
 	server: {
 		host: true,
