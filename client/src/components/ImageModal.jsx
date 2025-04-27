6b289813c5372180726f3675d04b8ac76a2bdab8
yanghj313 <lawork313@gmail.com>
2025-04-27 15:33:25 +0900

    스와이프 부분분

--- client/src/components/ImageModal.jsx
+++ client/src/components/ImageModal.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '../assets/css/fullpage.css';
 
 const ImageModal = ({ images, startIndex = 0, onClose }) => {
+	const [isSwiping, setIsSwiping] = useState(false);
+
 	const settings = {
 		initialSlide: startIndex,
 		dots: true,
@@ -15,10 +17,18 @@ const ImageModal = ({ images, startIndex = 0, onClose }) => {
 		speed: 500,
 		swipe: true,
 		draggable: true,
+		beforeChange: () => setIsSwiping(true),
+		afterChange: () => setTimeout(() => setIsSwiping(false), 300),
+	};
+
+	const handleOverlayClick = () => {
+		if (!isSwiping) {
+			onClose();
+		}
 	};
 
 	return (
-		<div className="modal-overlay" onClick={onClose}>
+		<div className="modal-overlay" onClick={handleOverlayClick}>
 			<div className="modal-content" onClick={e => e.stopPropagation()}>
 				<button className="modal-close" onClick={onClose}>
 					X
