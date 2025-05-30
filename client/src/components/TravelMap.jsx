8cd9cb03cadb9a6aef0a47fd26c0060570dbbe28
yanghj313 <lawork313@gmail.com>
2025-05-30 16:05:17 +0900

    [update] 섹션4 지도 레이아웃 수정

--- client/src/components/TravelMap.jsx
+++ client/src/components/TravelMap.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import '../assets/css/fullpage.css';
 import { FaMapMarkerAlt } from 'react-icons/fa';
 
