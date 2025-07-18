d32e6f091a8b4dbd78e9bcd47f5d37869ff7c47b
yanghj313 <lawork313@gmail.com>
2025-07-18 11:07:27 +0900

    [update] 서브헤더 이미지 변경

--- client/src/components/SubHeader.jsx
+++ client/src/components/SubHeader.jsx
@@ -3,26 +3,45 @@ import '../assets/css/SubHeader.css';
 
 const SubHeader = ({ title = '서브 타이틀', backgroundImage }) => {
 	return (
-		<header className="sub-header-simple" style={backgroundImage ? { backgroundImage: `url(${backgroundImage})` } : {}}>
-			<h1 className="sub-header-title">{title}</h1>
-
-			{/* 주황빛 3겹 파도 */}
-			<div className="wave">
-				<svg className="gnb_wave" viewBox="0 0 1200 100" preserveAspectRatio="none">
+		<header className="subTop" style={backgroundImage ? { backgroundImage: `url(${backgroundImage})` } : {}}>
+			{/* 상단 파도 (SVG) */}
+			<div className="wave_before">
+				<svg className="gnb_wave" viewBox="0 24 150 28" preserveAspectRatio="none">
 					<defs>
 						<path
 							id="gentle-wave"
-							d="
-								M0,30 
-								C150,90 350,-30 600,30 
-								C850,90 1050,-30 1200,30 
-								L1200,100 L0,100 Z"
+							d="M-160 44c30 0 
+								58-18 88-18s
+								58 18 88 18 
+								58-18 88-18 
+								58 18 88 18
+								v44h-352z"
 						/>
 					</defs>
 					<g className="parallax">
-						<use href="#gentle-wave" fill="rgba(255,163,100,0.3)" className="wave1" />
-						<use href="#gentle-wave" fill="rgba(255,122,89,0.5)" className="wave2" />
-						<use href="#gentle-wave" fill="rgba(255,85,70,0.8)" className="wave3" />
+						<use xlinkHref="#gentle-wave" x="50" y="6" fill="rgba(255, 163, 100, 0.5)" />
+					</g>
+				</svg>
+			</div>
+
+			<h1 className="sub-header-title">{title}</h1>
+
+			{/* 하단 파도 (SVG) */}
+			<div className="wave_after">
+				<svg className="gnb_wave" viewBox="0 24 150 28" preserveAspectRatio="none" style={{ transform: 'rotate(180deg)' }}>
+					<defs>
+						<path
+							id="gentle-wave"
+							d="M-160 44c30 0 
+								58-18 88-18s
+								58 18 88 18 
+								58-18 88-18 
+								58 18 88 18
+								v44h-352z"
+						/>
+					</defs>
+					<g className="parallax">
+						<use xlinkHref="#gentle-wave" x="50" y="6" fill="rgba(255, 85, 70, 0.6)" />
 					</g>
 				</svg>
 			</div>
