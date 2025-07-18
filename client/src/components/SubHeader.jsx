6f102ca36405bfe2b1b97b3cad434dea2f9cadf4
yanghj313 <lawork313@gmail.com>
2025-07-18 11:20:57 +0900

    [update] 서브헤더 이미지 변경

--- client/src/components/SubHeader.jsx
+++ client/src/components/SubHeader.jsx
@@ -5,26 +5,6 @@ const SubHeader = ({ title = '서브 타이틀', backgroundImage }) => {
 	return (
 		<header className="subTop" style={backgroundImage ? { backgroundImage: `url(${backgroundImage})` } : {}}>
 			<h1 className="sub-header-title">{title}</h1>
-
-			{/* 한 겹 파도 */}
-			<div className="wave_one">
-				<svg className="gnb_wave" viewBox="0 24 150 28" preserveAspectRatio="none">
-					<defs>
-						<path
-							id="gentle-wave"
-							d="M-160 44c30 0 
-                58-18 88-18s
-                58 18 88 18 
-                58-18 88-18 
-                58 18 88 18
-                v44h-352z"
-						/>
-					</defs>
-					<g className="parallax">
-						<use xlinkHref="#gentle-wave" x="50" y="6" fill="#ff5722" />
-					</g>
-				</svg>
-			</div>
 		</header>
 	);
 };
