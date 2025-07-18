04e4e8ca55327e1f757145267aefa39e96ea30c5
yanghj313 <lawork313@gmail.com>
2025-07-18 10:48:43 +0900

    [update] 서브페이지 상하 파도

--- client/src/components/SubHeader.jsx
+++ client/src/components/SubHeader.jsx
@@ -6,16 +6,39 @@ const SubHeader = ({ title = '서브 타이틀', backgroundImage }) => {
 		<header className="sub-header-simple" style={backgroundImage ? { backgroundImage: `url(${backgroundImage})` } : {}}>
 			<h1 className="sub-header-title">{title}</h1>
 
-			{/* 아래가 곡선으로 마감된 파도 */}
+			{/* 곡선으로 상하 마감된 주황빛 SVG 파도 */}
 			<div className="wave">
-				<svg className="wave-layer" viewBox="0 0 1440 320" preserveAspectRatio="none">
-					<path className="wave1" d="M0,160 C360,320 1080,0 1440,160 C1080,320 360,0 0,160 Z" />
-				</svg>
-				<svg className="wave-layer" viewBox="0 0 1440 320" preserveAspectRatio="none">
-					<path className="wave2" d="M0,180 C360,300 1080,60 1440,180 C1080,300 360,60 0,180 Z" />
-				</svg>
-				<svg className="wave-layer" viewBox="0 0 1440 320" preserveAspectRatio="none">
-					<path className="wave3" d="M0,200 C360,280 1080,120 1440,200 C1080,280 360,120 0,200 Z" />
+				<svg className="gnb_wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 100" preserveAspectRatio="none">
+					<defs>
+						<path
+							id="gentle-wave"
+							d="
+                M-160 44
+                C-130 44 -102 26 -72 26
+                S-14 44 16 44
+                S74 26 104 26
+                S162 44 192 44
+                S250 26 280 26
+                S338 44 368 44
+                S426 26 456 26
+                S514 44 544 44
+
+                C514 64 456 64 456 64
+                S368 64 368 64
+                S280 64 280 64
+                S192 64 192 64
+                S104 64 104 64
+                S16 64 16 64
+                S-72 64 -72 64
+                S-160 64 -160 64
+                Z"
+						/>
+					</defs>
+					<g className="parallax">
+						<use xlinkHref="#gentle-wave" x="0" y="0" fill="rgba(255, 163, 100, 0.3)" />
+						<use xlinkHref="#gentle-wave" x="0" y="5" fill="rgba(255, 122, 89, 0.5)" />
+						<use xlinkHref="#gentle-wave" x="0" y="10" fill="rgba(255, 85, 70, 0.8)" />
+					</g>
 				</svg>
 			</div>
 		</header>
