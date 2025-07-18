b879a80576eafadd63f746ebddeff31314fb017d
yanghj313 <lawork313@gmail.com>
2025-07-18 09:24:07 +0900

    [update] 서브페이지 파도 수정

--- client/src/components/SubHeader.jsx
+++ client/src/components/SubHeader.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import '../assets/css/SubHeader.css';
 
-const SubHeader = ({ title = '서브페이지 타이틀', backgroundImage = '/img/main_img/onsen_bg.jpg' }) => {
+const SubHeader = ({ title = '서브페이지 타이틀', backgroundImage }) => {
 	return (
-		<header className="sub-header-simple" style={{ backgroundImage: `url(${backgroundImage})` }}>
+		<header className="sub-header-simple" style={backgroundImage ? { backgroundImage: `url(${backgroundImage})` } : {}}>
 			<h1 className="sub-header-title">{title}</h1>
-
-			{/* 바다와 파도 */}
-
-			<div className="wave"></div>
-			<div className="wave second"></div>
+			<div className="wave-container">
+				<div className="wave wave1"></div>
+				<div className="wave wave2"></div>
+			</div>
 		</header>
 	);
 };
