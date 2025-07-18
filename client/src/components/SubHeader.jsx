dffc4884303c62647e2764f40fe122cf6af7e33b
yanghj313 <lawork313@gmail.com>
2025-07-18 09:13:54 +0900

    [update] 서브헤더 재수정(파도 위치 수정)

--- client/src/components/SubHeader.jsx
+++ client/src/components/SubHeader.jsx
@@ -3,16 +3,15 @@ import '../assets/css/SubHeader.css';
 
 const SubHeader = ({ title = '서브페이지 타이틀', backgroundImage = '/img/main_img/onsen_bg.jpg' }) => {
 	return (
-		<>
-			<header className="sub-header-simple" style={{ backgroundImage: `url(${backgroundImage})` }}>
-				<h1 className="sub-header-title">{title}</h1>
-			</header>
+		<header className="sub-header-simple" style={{ backgroundImage: `url(${backgroundImage})` }}>
+			<h1 className="sub-header-title">{title}</h1>
 
+			{/* 바다와 파도 */}
 			<div className="ocean">
 				<div className="wave"></div>
 				<div className="wave"></div>
 			</div>
-		</>
+		</header>
 	);
 };
 
